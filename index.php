dcc236b1c7ee64bf77c126d7f9c7be278c411608
Batman <batman@example.com>
2025-12-17 13:11:13 +0700

    add

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
-<h1>Hello World</h1>
-<h2> This is my first PHP file</h2>
 <?php
-    echo "This is printed using PHP!";
+    include_once "config.php";
+    
+    $sql = "select * from products";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    $products = $stmt->fetchAll(PDO::FETCH_OBJ);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>hello</p>
+    <h1>Trang sản phẩm</h1>
+    <table border="1">
+        <tr>
+            <td>id</td>
+            <td>name</td>
+            <td>price</td>
+        </tr>
+        <?php
+            foreach($products as $i){
+                ?>
+                    <tr>
+                        <td><?php echo $i->id ?></td>
+                        <td><?php echo $i->name ?></td>
+                        <td><?php echo $i->price ?></td>
+                    </tr>
+                <?php
+            }
+        ?>
+
+
+
+    </table>
+</body>
+</html>
