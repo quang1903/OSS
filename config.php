d0cd1b3c5751df8d1a9b8fd4f3b20a2a7e159d4a
Batman <batman@example.com>
2025-12-18 11:51:12 +0700

    fix mysql host for infinityfree

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
     try{
-        $pdo = new PDO("mysql:host=localhost; dbname=oss", "root", "");
+        $pdo = new PDO("mysql:host=sql309.infinityfree.com; dbname=oss", "if0_40701327", "1234567890Ghjkl");
         $pdo->query("set names utf8");
     }catch(Exception $e){
         echo $e->getMessage();
         exit();
     }
 ?>
+
